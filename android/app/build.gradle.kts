fb14d4ec6b5efb26e4011253407d59bcf827d325
Ibrahim Haitham <133378045+Ibrahim-40ou@users.noreply.github.com>
2026-05-06 16:46:42 +0300

    changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.wikipedia_webview"
+    namespace = "com.vodex.previewer"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.wikipedia_webview"
+        applicationId = "com.vodex.previewer"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/vodex/previewer/MainActivity.kt
+++ android/app/src/main/kotlin/com/vodex/previewer/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.wikipedia_webview
+package com.vodex.previewer
 
 import io.flutter.embedding.android.FlutterActivity
 
